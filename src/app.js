2010f4034a5b5ed3d0a5ac3c2a8cf96a04316670
InnaSofia <inna.artemieva.mironova@mail.ru>
2023-08-29 18:01:26 +0300

    css

--- src/app.js
+++ src/app.js
@@ -1,1 +1,37 @@
-import { format } from "date-fns";
+/* var accordion = function(){
+    var data = $(".accordion").attr("data-accordion");
+    
+    $(".accordion-header").on("clic", function(){
+     if(data === "cloce"){
+         $(".accordion-body").slideUp();
+         if($(this).hasClass("active")){
+             $(this).toggleClass("active");
+         
+         }else{
+             $(".accordion-header").removeClass("active");
+             $(this).toggleClass("active");
+         }
+         }
+         else{
+         $(this).toggleClass("active");
+     }
+     $(this).next(".accordion-body").not(":animated").slideToggle();
+    });
+ }
+ accordion()
+ */
+ 
+  /* const Modalcontainer1 = document.getElementById('Modalcontainer1');//Модальное окно1
+  const openModal1 = document.getElementById('openModal1');//кнопка1
+  
+  buttonModal1.addEventListener('clic',openModal1); */
+
+
+  function openModal(){
+    const Container = document.getElementById('Container')
+
+    Container.style.display = "flex"
+    
+}
+
+
