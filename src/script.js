93f5e8441ac2d4b7be5be4c3f1f4b5f2fc19fbf3
InnaSofia <inna.artemieva.mironova@mail.ru>
2023-09-27 19:46:56 +0300

    js

--- src/script.js
+++ src/script.js
@@ -1,18 +1,2 @@
- let isOpen = false
- const list = document.getElementById('list');
-
-
-function openModal(){
-    const myContainer = document.getElementById('myContainer');
-
-    if(isOpen){
-        myContainer.styles.display = "none"
-        isOpen = false
-    }else{
-        myContainer.styles.display = "flex"
-        isOpen = true
-    }
-} 
-
-
-
+ const questions = document.querySelectorAll('.question');
+ 
