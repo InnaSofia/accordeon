63c421d6f32620f0c82794048a41412047fc6ad4
InnaSofia <inna.artemieva.mironova@mail.ru>
2023-09-27 19:55:51 +0300

    оы

--- src/script.js
+++ src/script.js
@@ -1,2 +1,10 @@
- const questions = document.querySelectorAll('.question');
+ const openModal = document.getElementById('openModal');//открыть и закрыть
+ const answer = document.getElementById('answer');//ответ которы нужно открыть и закрыть
+
+ openModal.addEventListener('click',openModal);
+
+ function openAnswer(){
+    answer.style.display ="flex"
+ }
  
+ openAnswer()
