cff80f348f42169285d6866fc3ddde311a2b2499
InnaSofia <inna.artemieva.mironova@mail.ru>
2023-09-28 21:15:39 +0300

    открывает и закрывает вопросы

--- src/script.js
+++ src/script.js
@@ -1,30 +1,71 @@
-/*  const openModal = document.getElementById('openModal');//открыть и закрыть
- const answer = document.getElementById('answer');//ответ которы нужно открыть и закрыть
+const array =[
 
- openModal.addEventListener('click',openModal);
+    {
+        question: "Что такое Госуслуги?",
+        answer:"Госуслуги — портал, где граждане, организации и ИП могут оформить документы, получить выписки и справки в электронном виде, найти юридически значимую информацию."
+    },
 
- function openAnswer(){
-    answer.style.display ="flex"
- }
- 
- openAnswer() */
+    {
+        question: "Как зарегистрироваться на Госуслугах?",
+        answer:"Онлайн через банк — 10 минут, Лично в центре обслуживания — от 15 минут, При помощи номера телефона и электронной почты."
+    },
 
- /* let isOpen = false
-function openanswer(){
-   
+    {
+        question: "Куда подавать документы для смены ФИО?",
+        answer:"В загс по месту жительства или регистрации рождения."
+    },
 
-    const answer = document.getElementById('answer');
+    {
+        question: "Сколько времени занимает регистрация имени?",
+        answer:"Один месяц, если все необходимые документы были выданы отделом ЗАГС, в который вы обратились. Если нужные документы были выданы другими отделами ЗАГС, срок может быть увеличен до трёх месяцев."
+    },
 
+    {
+        question: "Что такое СНИЛС и зачем он нужен?",
+        answer:"СНИЛС — страховой номер индивидуального лицевого счёта,который присваивается каждому гражданину Социальным фондом (СФР). Страховой номер состоит из 11 цифр и присваивается один раз. Он не меняется даже при утере документов или смене ФИО. СНИЛС используется для пенсионных отчислений, а также для регистрации на Госуслугах и сайтах ведомств. С его помощью можно подавать заявления и получать электронные услуги."
+    },
 
-    if(isOpen){
-        answer.style.display = "none"
-        isOpen = false
-        
-    }else{
-        answer.style.display = "flex"
-        isOpen = true
-        
-    } */
+    {
+        question: "Как оформить СНИЛС?",
+        answer:"СНИЛС можно оформить у работодателя, на сайте Социального фонда (СФР) или лично в отделении СФР или МФЦ. Страховой номер присваивается один раз и не меняется даже при утере документов или смене ФИО."
+    },
 
-    
+    {
+        question: "Что делать если СНИЛС утерян?",
+        answer:"СНИЛС не имеет физического носителя — достаточно знать свой номер, поэтому утрата старой пластиковой карточки или распечатки с номером ничем не грозит. Ваши данные хранятся в системе Социального фонда (СФР), а узнать СНИЛС можно разными способами."
+    },
+]
+
+function renderArray(){
+    const container = document.getElementById('list')
+
+    container.innerHTML = ''
+
+    array.forEach((arrayItem) =>{
+        container.innerHTML += `
+        <div class="mt-2">
+                  <div id="question" class="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-700">
+                      <div><span>
+                      ${arrayItem.question}
+                      </span></div>
+                      <div><button id="button"><i class="fa-solid fa-lock" style="color: #7b0f99;"></i><button></div>
+                      </div>
+                  <div id="answer" class="px-4 pt-4 pb-2 text-sm text-gray-500 Modal hidden">
+                  ${arrayItem.answer}
+                  </div>
+              </div>
+        `
+    })
 }
+renderArray()
+
+const questions = document.querySelectorAll('#question')//все вопросы
+const answers = document.querySelectorAll('#answer')//все ответы
+
+//добавим обработчики событий для каждого вопросы
+questions.forEach((question, index, ) =>{
+    question.addEventListener('click',() =>{
+        answers[index].classList.toggle('hidden') //находим класс(тогд будет включать и переключать)
+        
+    })
+})
